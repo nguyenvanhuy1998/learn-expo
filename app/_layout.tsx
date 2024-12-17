e34918316f2384a774b60faba9004e5311ee2bee
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-12-17 14:58:40 +0700

    Add local file fonts with useFonts hooks

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 
+SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
-  return <Stack />;
+    const [loaded, error] = useFonts({
+        "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    });
+    useEffect(() => {
+        if (loaded || error) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded, error]);
+    if (!loaded && !error) {
+        return null;
+    }
+    return <Stack />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
 import { Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text
+                style={{
+                    fontFamily: "Poppins-Black",
+                }}
+            >
+                Edit app/index.tsx to edit this screen.
+            </Text>
+        </View>
+    );
 }
